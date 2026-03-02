1f0e855e5efc7e38a6c2a595081f66aa2701bf30
Jake Stanley <mail@jakestanley.co.uk>
2026-03-02 11:24:36 +0000

    add git to home.nix

--- home/jake/home.nix
+++ home/jake/home.nix
@@ -30,5 +30,41 @@
         EDITOR = "vim";
       };
     };
+    git = {
+      enable = true;
+          
+      userName = "Jake Stanley";
+      userEmail = "mail@jakestanley.co.uk";
+
+      extraConfig = {
+        core = {
+          editor = "vim";
+          whitespace = "trailing-space,space-before-tab,indent-with-non-tab";
+          excludesfile = "~/.gitignore_global";
+          autocrlf = "input";
+        };
+
+        push.default = "simple";
+
+        init.defaultBranch = "main";
+      };
+
+      aliases = {
+        ll = "!x() { git --no-pager log --pretty=tformat:\"%Cred%h %Cgreen%s %Cblue(%cn, %cr)\" --decorate -n$1;}; x";
+        url = "remote get-url --push origin";
+
+        co = "checkout";
+        cb = "checkout -b";
+        db = "branch -D";
+
+        s = "status";
+
+        eat = "stash --include-untracked";
+        poop = "stash pop";
+
+        pullr = "pull --rebase";
+        amend = "commit --amend";
+      };
+    };
   };
 }
