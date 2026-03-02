c4e69151b3e9d7bf20569d521f37e108ba0b4867
Jake Stanley <mail@jakestanley.co.uk>
2026-03-02 13:09:03 +0000

    shrike: use plasma-manager for previous config

--- home/jake/hosts/shrike.nix
+++ home/jake/hosts/shrike.nix
@@ -1,8 +1,18 @@
 { ... }:
 
 {
-  xdg.configFile = {
-    "kscreenlockerrc".source = ./../files/hosts/shrike/kscreenlockerrc;
-    "powerdevilrc".source = ./../files/hosts/shrike/powerdevilrc;
+
+  # networking.firewall.enable = false;
+
+  programs.plasma = {
+    enable = true;
+    configFile = {
+      "kscreenlockerrc"."Daemon"."LockOnResume" = false;
+      "powerdevilrc"."AC][Display" = {
+        "TurnOffDisplayIdleTimeoutSec" = -1;
+        "TurnOffDisplayWhenIdle" = false;
+      };
+      "powerdevilrc"."AC][SuspendAndShutdown"."AutoSuspendAction" = 0;
+    };
   };
 }
--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -9,6 +9,9 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "hm-backup";
+    sharedModules = [
+      inputs.plasma-manager.homeModules.plasma-manager
+    ];
     users.jake = import ../../home/jake/home.nix;
   };
 }
