e3d854dd1915b6ade7363c18d6b0c7d20655e9b2
Jake Stanley <mail@jakestanley.co.uk>
2026-03-08 19:33:37 +0000

    configure vim in nix properly

--- home/jake/common/editor.nix
+++ home/jake/common/editor.nix
@@ -1,5 +1,22 @@
 { ... }:
 
 {
-  home.file.".vimrc".source = ./config/vimrc;
+  programs.vim = {
+    defaultEditor = true;
+    enable = true;
+
+    extraConfig = ''
+      set nocompatible              " be iMproved, required
+      filetype off                  " required
+
+      set number
+      set colorcolumn=80
+      set ts=4 sw=4
+      syntax enable
+      " colo elflord
+
+      " tabs, etc (learned from http://stackoverflow.com/questions/1878974/redefine-tab-as-4-spaces)
+      set tabstop=8 softtabstop=0 shiftwidth=4 expandtab smarttab
+    '';
+  };
 }
