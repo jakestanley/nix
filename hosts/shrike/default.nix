42673d6e5010279bea375c76c6a6b953fd3cb521
Jake Stanley <mail@jakestanley.co.uk>
2026-03-08 19:52:52 +0000

    authorized keys are host specific (for now)

--- hosts/shrike/default.nix
+++ hosts/shrike/default.nix
@@ -29,6 +29,10 @@ in
 
   networking.hostName = "shrike";
 
+  users.users.jake.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0q1CwSf4NG0jPtBtWabETld24LR2QsIB4XQLpukXSK jake@Jacobs-MacBook-Pro.local"
+  ];
+
   home-manager.extraSpecialArgs = {
     hostname = "shrike";
   };
--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -9,8 +9,4 @@
       PermitRootLogin = "no";
     };
   };
-
-  users.users.jake.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0q1CwSf4NG0jPtBtWabETld24LR2QsIB4XQLpukXSK jake@Jacobs-MacBook-Pro.local"
-  ];
 }
