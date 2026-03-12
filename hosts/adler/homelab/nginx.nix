829573558c67e01e88636fb1f7de21c0d1ff3ace
Jake Stanley <mail@jakestanley.co.uk>
2026-03-12 07:08:14 +0000

    what's the point actually now

--- hosts/adler/homelab/nginx.nix
+++ hosts/adler/homelab/nginx.nix
@@ -1,15 +1,15 @@
 { ... }:
 
 {
-    services.nginx.virtualHosts = lib.mapAttrs (name: cfg: {
-        serverName = "${name}.stanley.arpa";
-        forceSSL = true;
-        # sslCertificate = "" # TODO
-        locations."/" = {
-            proxyPass = "http://${cfg.host}:${toString cfg.port}";
-            proxyWebsockets = true;
-        };
-        locations."= /healthz".return = "200";
-    }) config.homelab.services;
+    # services.nginx.virtualHosts = lib.mapAttrs (name: cfg: {
+    #     serverName = "${name}.stanley.arpa";
+    #     forceSSL = true;
+    #     # sslCertificate = "" # TODO
+    #     locations."/" = {
+    #         proxyPass = "http://${cfg.host}:${toString cfg.port}";
+    #         proxyWebsockets = true;
+    #     };
+    #     locations."= /healthz".return = "200";
+    # }) config.homelab.services;
 }
 
