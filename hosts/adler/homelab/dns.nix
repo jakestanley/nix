6caf8ba876ad706c247a43b5c841b96850ae5e76
Jake Stanley <mail@jakestanley.co.uk>
2026-03-12 10:18:02 +0000

    dns test, first hand written nix test

--- hosts/adler/homelab/dns.nix
+++ hosts/adler/homelab/dns.nix
@@ -1,9 +1,6 @@
 { lib, registry, ... }:
 
-# hosts:
-#   adler:
-#     hostname: adler.stanley.arpa
-#     ip: 10.92.8.6
+
 
 #   shed:
 #     hostname: shed.stanley.arpa
@@ -13,13 +10,29 @@
 #     hostname: shrike.stanley.arpa
 #     ip: 10.92.8.4
 
-{
+{   
+    # Nix way is to let Nix handle the config, so unlike on Ubuntu we won't have many files
     services.dnsmasq = {
         enable = true;
         settings = {
+            # hosts:
+            #   adler:
+            #     hostname: adler.stanley.arpa
+            #     ip: 10.92.8.6
             host-record = lib.mapAttrsToList
-            (name: cfg: "${name}.stanley.arpa,${cfg.ip}")
+            (name: host: "${name}.stanley.arpa,${host.ip}")
             registry.hosts;
         };
     };
+
+    # raw example (no functions)
+    # services.dnsmasq = {
+    #   enable = true;
+    #   settings = {
+    #     host-record = [
+    #       "adler.stanley.arpa,10.66.6.6"
+    #       "shrike.stanley.arpa,10.66.6.7"
+    #     ];
+    #   };
+    # };
 }
--- tests/dns.nix
+++ tests/dns.nix
@@ -1,28 +1,29 @@
 # tests/dns.nix
+# you'll need kvm access
+# - `sudo usermod -aG kvm $USER`
+# - log out, then back in and check `groups`
+# - `nix --extra-experimental-features "nix-command" build --file tests/dns.nix -L` (-L flag prevents logs being truncated)
+# - nix caches inputs by hash, so if nothing changed and the test passed then it won't run and just return the last exit code. Add --rebuild
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.testers.runNixOSTest {
   name = "dnsmasq-records";
 
   nodes.machine = { ... }: {
-    services.dnsmasq = {
-      enable = true;
-      settings = {
-        host-record = [
-          "adler.stanley.arpa,10.92.8.6"
-          "shrike.stanley.arpa,10.92.8.4"
-        ];
+
+    _module.args.registry = {
+      hosts = {
+        adler = { ip = "10.66.6.6"; };
+        shrike = { ip = "10.66.6.7"; };
       };
     };
+
+    environment.systemPackages = [ pkgs.dig ];
+
+    imports = [ ../hosts/adler/homelab/dns.nix ];
   };
 
-  testScript = ''
-    machine.wait_for_unit("dnsmasq.service")
-
-    print(machine.succeed("find /etc/dnsmasq.d/ -type f -exec cat {} +"))
-    print(machine.succeed("cat /etc/dnsmasq.conf"))
-
-    machine.succeed("cat /etc/dnsmasq.conf")
-    machine.succeed("dig @127.0.0.1 adler.stanley.arpa | grep 10.92.8.6")
-  '';
+  testScript = { nodes, ... }: ''
+    dnsmasq_conf = "${nodes.machine.services.dnsmasq.configFile}"
+  '' + builtins.readFile ./dns.py;
 }
