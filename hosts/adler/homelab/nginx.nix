233a96c41531ccc28252af1d96081caa624a162c
Jake Stanley <mail@jakestanley.co.uk>
2026-03-12 22:43:34 +0000

    nginx: extra config

--- hosts/adler/homelab/nginx.nix
+++ hosts/adler/homelab/nginx.nix
@@ -1,5 +1,15 @@
 { lib, registry, ... }:
 
+let
+  commonProxyConfig = ''
+    proxy_http_version 1.1;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+  '';
+in
+
 {
     services.nginx.enable = true;
 
@@ -11,23 +21,27 @@
 #       port: 20031
 #       scheme: http
 
-    services.nginx.virtualHosts = lib.mapAttrs (name: cfg: {
-        serverName = "${name}.stanley.arpa";
-        forceSSL = true;
-        sslCertificate = "/etc/homelab/certs/live/wildcard_stanley_arpa/fullchain.pem";
-        sslCertificateKey = "/etc/homelab/certs/live/wildcard_stanley_arpa/privkey.pem";
-        locations."/" = {
-            proxyPass = "http://${registry.hosts.${cfg.upstream.host}.ip}:${toString cfg.upstream.port}";
-            proxyWebsockets = true;
-        };
-        locations."= /healthz".return = "200";
-        
-    } // lib.optionalAttrs (name == "demucs") {
-        extraConfig = ''
-            client_max_body_size 256m;
-            proxy_read_timeout 300s;
-            proxy_send_timeout 300s;
-            send_timeout 300s;
-            '';
-        }) registry.services;
-}
+    services.nginx.virtualHosts = lib.mapAttrs (name: cfg:
+        lib.recursiveUpdate {
+            serverName = "${name}.stanley.arpa";
+            forceSSL = true;
+            sslCertificate = "/etc/homelab/certs/live/wildcard_stanley_arpa/fullchain.pem";
+            sslCertificateKey = "/etc/homelab/certs/live/wildcard_stanley_arpa/privkey.pem";
+            
+            locations."/" = {
+                proxyPass = "http://${registry.hosts.${cfg.upstream.host}.ip}:${toString cfg.upstream.port}";
+                proxyWebsockets = true;
+                extraConfig = commonProxyConfig;
+            };
+
+            locations."= /healthz".return = "200";
+        } (lib.optionalAttrs (name == "demucs") {
+            locations."/".extraConfig = commonProxyConfig + ''
+                    client_max_body_size 256m;
+                    proxy_read_timeout 300s;
+                    proxy_send_timeout 300s;
+                    send_timeout 300s;
+                '';
+            })
+        ) registry.services;
+    }
