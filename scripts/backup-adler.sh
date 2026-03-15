ce1c944e3bc9976b9a999c2fd102d7d1b13c956b
Jake Stanley <mail@jakestanley.co.uk>
2026-03-15 08:15:51 +0000

    don't accidentally back up root home you dummy

--- scripts/backup-adler.sh
+++ scripts/backup-adler.sh
@@ -16,7 +16,8 @@ sudo systemctl stop plexmediaserver
 
 echo "Stopped docker and plex. Beginning backup"
 
-BACKUP_DEST="$HOME/Dropbox/backups/adler"
+BACKUP_TARGET_HOME=/home/jake
+BACKUP_DEST="$BACKUP_TARGET_HOME/Dropbox/backups/adler"
 TIMESTAMP=$(date +%F_%H-%M)
 BACKUP_FILE="$BACKUP_DEST/adler-$TIMESTAMP.tar.gz"
 
@@ -24,28 +25,28 @@ mkdir -p "$BACKUP_DEST"
 
 sudo tar -czf "$BACKUP_FILE" \
     --warning=no-file-ignored \
-    --exclude="$HOME/.cache" \
-    --exclude="$HOME/.local/share/Trash" \
-    --exclude="$HOME/.local/pipx" \
-    --exclude="$HOME/.npm" \
-    --exclude="$HOME/.nvm" \
-    --exclude="$HOME/.codex" \
-    --exclude="$HOME/.dropbox" \
-    --exclude="$HOME/.dropbox-dist" \
-    --exclude="$HOME/.vscode-server" \
-    --exclude="$HOME/.cargo" \
-    --exclude="$HOME/node_modules" \
-    --exclude="$HOME/Downloads" \
-    --exclude="$HOME/Dropbox" \
-    --exclude="$HOME/.local/share/Steam" \
-    --exclude="$HOME/**/__pycache__" \
-    --exclude="$HOME/**/*.pyc" \
+    --exclude="$BACKUP_TARGET_HOME/.cache" \
+    --exclude="$BACKUP_TARGET_HOME/.local/share/Trash" \
+    --exclude="$BACKUP_TARGET_HOME/.local/pipx" \
+    --exclude="$BACKUP_TARGET_HOME/.npm" \
+    --exclude="$BACKUP_TARGET_HOME/.nvm" \
+    --exclude="$BACKUP_TARGET_HOME/.codex" \
+    --exclude="$BACKUP_TARGET_HOME/.dropbox" \
+    --exclude="$BACKUP_TARGET_HOME/.dropbox-dist" \
+    --exclude="$BACKUP_TARGET_HOME/.vscode-server" \
+    --exclude="$BACKUP_TARGET_HOME/.cargo" \
+    --exclude="$BACKUP_TARGET_HOME/node_modules" \
+    --exclude="$BACKUP_TARGET_HOME/Downloads" \
+    --exclude="$BACKUP_TARGET_HOME/Dropbox" \
+    --exclude="$BACKUP_TARGET_HOME/.local/share/Steam" \
+    --exclude="$BACKUP_TARGET_HOME/**/__pycache__" \
+    --exclude="$BACKUP_TARGET_HOME/**/*.pyc" \
     --exclude="*/.git" \
-    --exclude="$HOME/Music/Playlists" \
+    --exclude="$BACKUP_TARGET_HOME/Music/Playlists" \
     --exclude="*/.venv" \
     --exclude="*/venv" \
     --exclude="*/ipc-socket" \
-    /home/jake \
+    $BACKUP_TARGET_HOME \
     /etc/openvpn \
     /etc/homelab/certs \
     /var/lib/plexmediaserver
