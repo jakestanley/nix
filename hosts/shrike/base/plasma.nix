ab351a59f6ef169a40d199cdb1a8c7a1302fa46d
Jake Stanley <mail@jakestanley.co.uk>
2026-03-30 20:16:54 +0100

    shrike: restore plasma.nix, thought it was used for something

---
+++ hosts/shrike/base/plasma.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    elisa
+    spectacle
+    gwenview
+    okular
+    discover
+    ark
+    khelpcenter
+    krdp
+  ];
+}
