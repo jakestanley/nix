72e7a6d97aa6550af9136f30ce1a6ff89b09e703
Jake Stanley <mail@jakestanley.co.uk>
2026-04-02 16:03:00 +0100

    shrike: add gamemoderun to steam autostart path

--- hosts/shrike/conditionals/steam-autostart.nix
+++ hosts/shrike/conditionals/steam-autostart.nix
@@ -7,6 +7,7 @@
     after = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
     wantedBy = [ "graphical-session.target" ];
+    serviceConfig.Environment = "PATH=/run/current-system/sw/bin:/usr/bin:/bin";
     serviceConfig.ExecStart = if activeProfile == "tenfoot"
       then "${pkgs.steam}/bin/steam -tenfoot"
       else "${pkgs.steam}/bin/steam -silent";
