cd801e90506b1c4e6a5776e5f815c36577adf745
Jake Stanley <mail@jakestanley.co.uk>
2026-04-02 15:40:07 +0100

    shrike: uinput attempt

--- hosts/shrike/profiles/tenfoot.nix
+++ hosts/shrike/profiles/tenfoot.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, activeProfile, ... }:
 
 {
+  hardware.uinput.enable = true;
+  users.users.jake.extraGroups = [ "input" ];
+
   environment.systemPackages = lib.optionals (activeProfile == "tenfoot") (with pkgs; [
     # tenfoot-specific packages
   ]);
