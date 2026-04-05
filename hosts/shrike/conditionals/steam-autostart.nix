4ae6d4f2e2e678963922cdff14547aedff6368eb
Jake Stanley <mail@jakestanley.co.uk>
2026-04-05 09:17:59 +0100

    shrike: restore gamescope

--- hosts/shrike/conditionals/steam-autostart.nix
+++ hosts/shrike/conditionals/steam-autostart.nix
@@ -9,7 +9,7 @@
     wantedBy = [ "graphical-session.target" ];
     serviceConfig.Environment = "PATH=/run/current-system/sw/bin:/usr/bin:/bin";
     serviceConfig.ExecStart = if activeProfile == "tenfoot"
-      then "${pkgs.steam}/bin/steam -tenfoot"
+      then "${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 -w 3840 -h 2160 -f -e -- ${pkgs.steam}/bin/steam -tenfoot"
       else "${pkgs.steam}/bin/steam -silent";
   };
 }
