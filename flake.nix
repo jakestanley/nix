3da61f172f8d4149aed60da98a2275522b27f183
Jake Stanley <mail@jakestanley.co.uk>
2026-09-17 19:43:43 +0000

    fix(turing): pin nix-darwin to nix-darwin-26.05 release branch
    Homebrew disabled the `--cleanup` bundle switch; the previous unversioned
    pin still used it and broke activation. Pinning to the 26.05 release branch
    tracks the matching Nixpkgs release and uses `--force-cleanup` instead.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
